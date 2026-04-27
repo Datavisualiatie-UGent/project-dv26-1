20db583cb6dae1f5221cd4ce32331a29d9d85a34
Adriaan Jacquet <adriaanjacquet@gmail.com>
2026-04-27 14:24:02 +0200

    updated data script

--- data/getdata.sh
+++ data/getdata.sh
@@ -1,10 +1,13 @@
-wget https://zenodo.org/records/18613195/files/Glottography/asher2007world-v2.0.zip?download=1
-mv asher2007world-v2.0.zip\?download\=1 data.zip
+#!/bin/bash
 
-unzip data.zip
-rm data.zip
+wget -N https://zenodo.org/records/18613195/files/Glottography/asher2007world-v2.0.zip?download=1
+mv asher2007world-v2.0.zip\?download\=1 asher2007world-v2.0.zip
 
-mv Glottography-asher2007world-2010223/cldf/traditional/* ./traditional/
-mv Glottography-asher2007world-2010223/cldf/contemporary/* ./contemporary/
+unzip asher2007world-v2.0.zip -d asher2007world-v2.0
+
+rm asher2007world-v2.0.zip
+
+cp asher2007world-v2.0/Glottography-asher2007world-2010223/cldf/traditional/* ./traditional/
+cp asher2007world-v2.0/Glottography-asher2007world-2010223/cldf/contemporary/* ./contemporary/
 
 rm -rf Glottography-asher2007world-2010223/
