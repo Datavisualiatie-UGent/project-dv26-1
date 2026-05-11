a18842edbb8499a3370fee14565873c805065f36
Your Name <you@example.com>
2026-05-11 13:32:09 +0200

    wat deed dit her nog ik weet het niet meer

--- src/js/data.js
+++ src/js/data.js
@@ -1,4 +1,4 @@
 const data_csv = await fetch(new URL("../data/dataset", import.meta.url)).then(response => response.text());
-const lijnen = data_csv.split("\n"); //kankerwindows
+const lijnen = data_csv.split("\n");
 const hoofding = lijnen[0].split(",");
 export const data = lijnen.slice(1).map(line => line.split(",").reduce((acc, curr, index) => ({ [hoofding[index]]: curr, ...acc }), {}));
