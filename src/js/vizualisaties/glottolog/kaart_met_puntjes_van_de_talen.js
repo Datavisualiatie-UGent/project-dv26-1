8fb7c15b6c31bed25a3514d06513029354cc39b8
Your Name <you@example.com>
2026-05-11 14:22:39 +0200

    z

--- src/js/vizualisaties/glottolog/kaart_met_puntjes_van_de_talen.js
+++ src/js/vizualisaties/glottolog/kaart_met_puntjes_van_de_talen.js
@@ -76,7 +76,7 @@ svg.append("rect")
     .attr("width", width)
     .attr("height", height)
     .attr("rx", 10)
-    .attr("fill", "#4a4a4a")
+    .attr("fill", "#c5f0ff")
 
 svg.selectAll()
     .data(puntjes)
